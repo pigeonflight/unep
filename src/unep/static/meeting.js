388273a28724f253330bdb505be796ff5a1a92f0
pigeonflight <david@alteroo.com>
2014-05-06 23:53:28 +0000

    meeting js now uses a more specific selector;added more classes to meeting downloads page to facilitate styling

--- src/unep/static/meeting.js
+++ src/unep/static/meeting.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){    
   var collapse = new jQueryCollapse($('#meeting-texts'), { query: '> h2' });
 
-  $('#meeting-navigation a')
+  $('#meeting-texts h2 > a')
     .on('click', function(e) {
       if ($(this).attr('href').substr(0, 1) === '#') {
-        e.preventDefault();
+       // e.preventDefault();
         var selected = $('#meeting-texts').find($(this).attr('href'));
         collapse.close();
         collapse.open($('#meeting-texts h2').index(selected));
