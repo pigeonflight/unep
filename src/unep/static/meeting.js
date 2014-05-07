aa87f5cae0bb733e9444c2362502094c44cc9a4c
pigeonflight <david@alteroo.com>
2014-05-07 21:46:49 +0000

    added carets to the collapsible sections on the meeting page; updated layout of meeting downloads and meeting page

--- src/unep/static/meeting.js
+++ src/unep/static/meeting.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){    
   var collapse = new jQueryCollapse($('#meeting-texts'), { query: '> h2' });
  
+     $("#meeting-texts h2").on("click",function(e){
+       e.preventDefault();
+        clickeditem = $(this);
+        icon = clickeditem.find('i');
+        if (clickeditem.hasClass("open")) {
+            icon.removeClass("fa-caret-down")
+                  .addClass("fa-caret-right");
+            
+        } else {
+              icon.removeClass("fa-caret-right")
+              .addClass("fa-caret-down");
+        }
+       
+      
+  }); 
   $('#meeting-texts h2 > a')
     .on('click', function(e) {
       if ($(this).attr('href').substr(0, 1) === '#') {
