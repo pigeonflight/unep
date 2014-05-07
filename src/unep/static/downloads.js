a1880ecb316f28c930c751f55bb1b7920a4329a6
pigeonflight <david@alteroo.com>
2014-05-07 12:24:13 +0000

    added theme; and some changes to the download.js and meeting_downloads

--- src/unep/static/downloads.js
+++ src/unep/static/downloads.js
@@ -1,18 +1,43 @@
-$(document).ready(function(){   
+$(window).load(function(){   
     
-  var collapse = new jQueryCollapse($('#meeting-downloads'));
+ // var collapse = new jQueryCollapse($('#meeting-downloads'));
+ var docscollapse = $('#meeting-downloads').collapse({
+ show: function() {
+    console.log("hello show"); 
+    this.slideDown(100);
+  },
+  hide: function() {
+    this.slideUp(100);
+   },
+     });
 
+    $(".meeting-downloads-document-link").prepOverlay({
+    subtype: 'ajax',
+    filter: '#content>*',
+    formselector: 'form'
+    });
+    
+  $("#meeting-downloads").on("open", function(e, section) {
+  console.log(section, " is open");
+});
   $("#meeting-downloads h2 > a").on("click",function(e){
-      
+       e.preventDefault();
         icon = $(this).find('i');
-        if (icon.hasClass("fa-chevron-circle-right")) {
-            icon.removeClass("fa-chevron-circle-right");
-            icon.addClass("fa-chevron-circle-down");
-            
-        } else {
-              icon.removeClass("fa-chevron-circle-down");
-              icon.addClass("fa-chevron-circle-right");
+        if (icon.hasClass("fa-caret-right")) {
+            icon.removeClass("fa-caret-right")
+                  .addClass("fa-caret-down");
+                  return
+        } 
+       if (icon.hasClass("fa-caret-down")) {
+            icon.removeClass("fa-caret-down")
+                  .addClass("fa-caret-right");
+                   return
+        } 
+      /* else {
+              icon.removeClass("fa-caret-down")
+              .addClass("fa-caret-right");
         }
+        */
       
   });
   $('#meeting-downloads').find('input[type="checkbox"]')
@@ -26,4 +51,3 @@ $(document).ready(function(){
   $('.meeting-select-disabled input').attr('disabled', 'disabled');
 
 });
-
